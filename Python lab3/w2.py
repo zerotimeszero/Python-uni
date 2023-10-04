22f41302fcbc46a47b1c4a7aa40156f21433135c
zerotimeszero <136163980+zerotimeszero@users.noreply.github.com>
2023-10-04 15:27:42 +0700

    Update w2.py

--- Python lab3/w2.py
+++ Python lab3/w2.py
@@ -1,4 +1,3 @@
-#1
 '''
 from statistics import mean
 def sum_transitions(company, stat):
@@ -17,13 +16,52 @@ ad_stats = [
     '2023-01-03,ya,4625',
     '2023-01-03,ok,245',
 ]
-company = input()
-print(sum_transitions(company,ad_stats))
+company = input('Введите название кампании: ')
+result = sum_transitions(company,ad_stats)
+print(f"Минимальное - {result[0]}\nМаксимальное - {result[1]}\nСреднее - {result[-1]}")
 '''
 #2
+
 from Currency import Rate
-
-
+'''
+print("Пункты 1|2")
+print('------------------------------------------------\n')
 curr = Rate('full')
+print(curr.usd())
+print('\n------------------------------------------------')
+print('Пункт 3')
+print(Rate('wName').make_format('USD'))
+print('\n------------------------------------------------')
+print('Пункт 4')
 print(curr.usd(True))
-print(curr.get_currency_info('RSD'))
+print('\n------------------------------------------------')
+print('Пункт 5')
+print(curr.get_currency_info('EUR'))
+print('\n------------------------------------------------')
+print('Пункт 6')
+print(curr.max_currency())
+print(curr.min_currency())
+print('\n------------------------------------------------')
+print('Пункт 7')
+print(curr.usd_for_weeks(5))
+print('\n------------------------------------------------')
+print('Пункт 8')
+print(curr.currency_for_weeks('EUR'),5)
+'''
+
+from Matrix import Matrix
+
+matrix = Matrix(2,2)
+matrix.input_matrix()
+print('\nМатрица:\n')
+matrix.print_matrix()
+print('\n------------------------------------------------')
+print('Сумма i-ого столбца')
+print(matrix.column_sum(1))
+print('\n------------------------------------------------')
+print('Кол-во нулей')
+print(matrix.zero_count())
+matrix.set_diagonal(5)
+print('\n------------------------------------------------')
+print('Матрица после замены элементов главной диагонали')
+matrix.print_matrix()
